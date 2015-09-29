50bee49f8d69bc253e878ee015513a6d9521efb0
hmuronaka <capriccio27@gmail.com>
2015-09-29 16:52:20 +0900

    Initial Commit

---
+++ HMSugarKit/HMSugarKit.h
@@ -0,0 +1,19 @@
+//
+//  HMSugarKit.h
+//  HMSugarKit
+//
+//  Created by Muronaka Hiroaki on 2015/09/29.
+//  Copyright © 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMSugarKit.
+FOUNDATION_EXPORT double HMSugarKitVersionNumber;
+
+//! Project version string for HMSugarKit.
+FOUNDATION_EXPORT const unsigned char HMSugarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMSugarKit/PublicHeader.h>
+
+
