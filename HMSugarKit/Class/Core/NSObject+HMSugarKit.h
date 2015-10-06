561a793b24b3e39ffc8755553ae2193bf00b3716
hmuronaka <capriccio27@gmail.com>
2015-10-06 12:57:05 +0900

    Add NSObject+HMSugarKit

---
+++ HMSugarKit/Class/Core/NSObject+HMSugarKit.h
@@ -0,0 +1,18 @@
+//
+//  NSObject+HMSugarKit.h
+//  HMSugarKit
+//
+//  Created by MuronakaHiroaki on 2015/10/06.
+//  Copyright © 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^SKObjectBlock)();
+typedef void (^SKObjectCancelBlock)(BOOL cancel);
+
+@interface NSObject (HMSugarKit)
+
+-(SKObjectCancelBlock)sk_performBlockOnMainThread:(SKObjectBlock)block;
+
+@end
