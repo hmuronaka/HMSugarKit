7b923877e5a988af3ef121b0ee067f242d1465e0
hmuronaka <capriccio27@gmail.com>
2015-10-25 10:51:47 +0900

    Add UIImage category

---
+++ HMSugarKit/Class/UIKit/UIImage+HMSugarKit.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+HMSugarKit.h
+//  HMSugarKit
+//
+//  Created by Muronaka Hiroaki on 2015/10/25.
+//  Copyright © 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (HMSugarKit)
+
+-(UIImage*)sk_resizeWithWidthRate:(CGFloat)widthRate heightRate:(CGFloat)heightRate;
+
+@end
